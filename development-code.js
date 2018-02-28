de2a0368f0326d60a288499867ff588adfcd3fbb
smd80p <36929182+smd80p@users.noreply.github.com>
2018-02-28 17:35:18 +0000

    Initial commit of local development tools

---
+++ development-code.js
@@ -0,0 +1,53 @@
+
+
+
+var development_host_and_port = "127.0.0.1:8080";
+var css_file = "theme.css";
+
+var css_url = "http://" + development_host_and_port + "/" + css_file;
+var livereload_url = "ws://" + development_host_and_port + "/ws";
+
+function addCss(fileName) {
+
+    var head = document.head;
+    var link = document.createElement("link");
+
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = fileName;
+
+    head.appendChild(link);
+}
+
+addCss(css_url);
+
+
+// <![CDATA[  <-- For SVG support
+if ('WebSocket' in window) {
+    (function() {
+        function refreshCSS() {
+            var sheets = [].slice.call(document.getElementsByTagName("link"));
+            var head = document.getElementsByTagName("head")[0];
+            for (var i = 0; i < sheets.length; ++i) {
+                var elem = sheets[i];
+                head.removeChild(elem);
+                var rel = elem.rel;
+                if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+                    var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+                    elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+                }
+                head.appendChild(elem);
+            }
+        }
+
+        var address = livereload_url;
+        var socket = new WebSocket(address);
+        socket.onmessage = function(msg) {
+            if (msg.data == 'reload') window.location.reload();
+            else if (msg.data == 'refreshcss') refreshCSS();
+        };
+        console.log('Live reload enabled.');
+    })();
+}
+// ]]>
+
